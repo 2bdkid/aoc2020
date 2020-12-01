f181ab917d50c9a7f065a91ae3534adaf3abb191
Brady Dean <2bdkid@gmail.com>
2020-12-01 18:51:39 -0500

    use to_vec rather than from_iter

--- src/day1.rs
+++ src/day1.rs
@@ -86,18 +86,18 @@ pub fn solve_part2_btreeset(input: &[u64]) -> Option<u64> {
 
 #[aoc(day1, part2, THREESUM)]
 pub fn solve_part2_3sum(input: &[u64]) -> Option<u64> {
-    let mut input = Vec::from_iter(input.iter());
+    let mut input = input.to_vec();
     input.sort();
     let n = input.len();
 
     for i in 0..n-2 {
-        let a = *input[i];
+        let a = input[i];
         let mut start = i;
         let mut end = n - 1;
 
         while start <= end {
-            let b = *input[start];
-            let c = *input[end];
+            let b = input[start];
+            let c = input[end];
             if a + b + c == 2020 {
                 return Some(a * b * c);
             }
