7e33677381eb30787e175c081bef37945a7b3999
Brady Dean <2bdkid@gmail.com>
2020-12-11 22:58:41 -0500

    added day11 part2 solution

--- src/day11.rs
+++ src/day11.rs
@@ -90,3 +90,162 @@ pub fn solve_part1(input: &HashMap<(i32, i32), Position>) -> usize {
         .filter(|p| **p == Position::Occupied)
         .count()
 }
+
+fn count_adjacent_occupied_seats(pt: (i32, i32), grid: &HashMap<(i32, i32), Position>) -> u32 {
+    let (x, y) = pt;
+    let mut acc = 0;
+
+    // up
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x, y - i)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // down
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x, y + i)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // left
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x - i, y)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // right
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x + i, y)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // top-right
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x + i, y - i)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // top-left
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x - i, y - i)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // bottom-left
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x - i, y + i)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // bottom-right
+    for i in 1.. {
+        if let Some(p) = grid.get(&(x + i, y + i)) {
+            if *p != Position::Floor {
+                if *p == Position::Occupied {
+                    acc += 1;
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    acc
+}
+
+#[aoc(day11, part2)]
+pub fn solve_part2(input: &HashMap<(i32, i32), Position>) -> usize {
+    let mut positions = input.clone();
+
+    loop {
+        let adj: HashMap<(i32, i32), u32> = positions
+            .iter()
+            .filter_map(|(pt, p)| {
+                if *p != Position::Floor {
+                    Some(*pt)
+                } else {
+                    None
+                }
+            })
+            .map(|p| (p, count_adjacent_occupied_seats(p, &positions)))
+            .collect();
+
+        let mut changes: Vec<((i32, i32), Position)> = Vec::new();
+
+        for (pt, occupied) in adj {
+            if *positions.get(&pt).unwrap() == Position::Empty && occupied == 0 {
+                changes.push((pt, Position::Occupied));
+            } else if *positions.get(&pt).unwrap() == Position::Occupied && occupied >= 5 {
+                changes.push((pt, Position::Empty));
+            }
+        }
+
+        if changes.is_empty() {
+            break;
+        }
+
+        positions.extend(changes.iter().copied());
+    }
+
+    positions
+        .values()
+        .filter(|p| **p == Position::Occupied)
+        .count()
+}
