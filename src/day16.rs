60b8e5786243bc9e053f7be2052fdcf9c336088c
Brady Dean <2bdkid@gmail.com>
2020-12-17 13:12:21 -0500

    changed Field to work with u64 and added day16 part2 solution

--- src/day16.rs
+++ src/day16.rs
@@ -1,24 +1,26 @@
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
 
-use std::collections::{HashMap, HashSet};
+use pathfinding::kuhn_munkres;
+use pathfinding::matrix::Matrix;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Field {
     name: String,
-    range1: (u32, u32),
-    range2: (u32, u32),
+    range1: (u64, u64),
+    range2: (u64, u64),
 }
 
 impl Field {
-    fn in_range(&self, value: u32) -> bool {
+    fn in_range(&self, value: u64) -> bool {
         let (a, b) = self.range1;
         let (x, y) = self.range2;
         value >= a && value <= b || value >= x && value <= y
     }
 }
 
-pub type Ticket = Vec<u32>;
+pub type Ticket = Vec<u64>;
 
 #[aoc_generator(day16)]
 fn input_generator(input: &str) -> (Vec<Field>, Ticket, Vec<Ticket>) {
@@ -27,10 +29,10 @@ fn input_generator(input: &str) -> (Vec<Field>, Ticket, Vec<Ticket>) {
         .filter_map(|l| {
             if l.contains("or") {
                 let name: String = l.chars().take_while(|c| *c != ':').collect();
-                let ranges: Vec<u32> = l
+                let ranges: Vec<u64> = l
                     .replace("-", " ")
                     .split_ascii_whitespace()
-                    .filter_map(|t| t.parse::<u32>().ok())
+                    .filter_map(|t| t.parse::<u64>().ok())
                     .collect();
                 Some(Field {
                     name,
@@ -49,8 +51,8 @@ fn input_generator(input: &str) -> (Vec<Field>, Ticket, Vec<Ticket>) {
             if l.contains(",") {
                 Some(
                     l.split(",")
-                        .map(|t| t.parse::<u32>().unwrap())
-                        .collect::<Vec<u32>>(),
+                        .map(|t| t.parse().unwrap())
+                        .collect::<Vec<u64>>(),
                 )
             } else {
                 None
@@ -61,12 +63,12 @@ fn input_generator(input: &str) -> (Vec<Field>, Ticket, Vec<Ticket>) {
     (fields, tickets[0].clone(), tickets[1..].to_vec())
 }
 
-fn completely_invalid_value(fields: &[Field], value: u32) -> bool {
+fn completely_invalid_value(fields: &[Field], value: u64) -> bool {
     fields.iter().all(|f| !f.in_range(value))
 }
 
 #[aoc(day16, part1)]
-pub fn solve_part1((fields, _, nearby_tickets): &(Vec<Field>, Ticket, Vec<Ticket>)) -> u32 {
+pub fn solve_part1((fields, _, nearby_tickets): &(Vec<Field>, Ticket, Vec<Ticket>)) -> u64 {
     nearby_tickets
         .iter()
         .map(|t| t.iter())
@@ -82,44 +84,10 @@ fn field_is_valid_for_index(field: &Field, index: usize, tickets: &[Ticket]) -> 
         .all(|value| field.in_range(value))
 }
 
-fn fill_index_mapping(
-    index: usize,
-    mapping: HashMap<String, usize>,
-    valid_fields_for_index: HashMap<usize, HashSet<String>>,
-) -> Option<HashMap<String, usize>> {
-    if index > 19 {
-        return Some(mapping.clone());
-    }
-
-    let valid_fields: HashSet<String> = valid_fields_for_index.get(&index).cloned().unwrap();
-
-    for field in valid_fields {
-        let mut new_mapping = mapping.clone();
-        new_mapping.insert(field.clone(), index);
-
-        let new_valid_fields_for_index: HashMap<usize, HashSet<String>> = valid_fields_for_index
-            .iter()
-            .map(|(index, fields)| {
-                let mut new_fields = fields.clone();
-                new_fields.remove(&field);
-                (*index, new_fields)
-            })
-            .collect();
-
-        if let Some(completed_mapping) =
-            fill_index_mapping(index + 1, new_mapping, new_valid_fields_for_index)
-        {
-            return Some(completed_mapping);
-        }
-    }
-
-    None
-}
-
 #[aoc(day16, part2)]
 pub fn solve_part2(
     (fields, your_ticket, nearby_tickets): &(Vec<Field>, Ticket, Vec<Ticket>),
-) -> u32 {
+) -> u64 {
     let tickets: Vec<Ticket> = nearby_tickets
         .iter()
         .chain(vec![your_ticket.clone()].iter())
@@ -130,26 +98,25 @@ pub fn solve_part2(
         .cloned()
         .collect();
 
-    let valid_fields_for_index: HashMap<usize, HashSet<String>> = (0..your_ticket.len())
-        .map(|index| {
-            (
-                index,
+    let costs: Matrix<i32> = Matrix::from_rows(
+        (0..your_ticket.len())
+            .map(|index| {
                 fields
                     .iter()
-                    .filter_map(|field| {
-                        if field_is_valid_for_index(field, index, &tickets) {
-                            Some(field.name.clone())
-                        } else {
-                            None
-                        }
-                    })
-                    .collect(),
-            )
-        })
-        .collect();
+                    .map(|field| field_is_valid_for_index(field, index, &tickets) as i32)
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>(),
+    )
+    .unwrap();
 
-    let mapping = fill_index_mapping(0, HashMap::new(), valid_fields_for_index).unwrap();
-    println!("{:?}", mapping);
+    let (_, assignments) = kuhn_munkres::kuhn_munkres(&costs);
+
+    let mapping: HashMap<String, usize> = assignments
+        .iter()
+        .enumerate()
+        .map(|(row, column)| (fields[*column].name.clone(), row))
+        .collect();
 
     your_ticket[*mapping.get("departure location").unwrap()]
         * your_ticket[*mapping.get("departure station").unwrap()]
