8296814bb8daa204d6d751fde5763268f83b8bab
Brady Dean <2bdkid@gmail.com>
2020-12-17 20:14:22 -0500

    added day17 part2 solution

--- src/day17.rs
+++ src/day17.rs
@@ -31,6 +31,44 @@ impl Point {
             })
             .collect()
     }
+
+    fn to_point4(&self, e4: i64) -> Point4 {
+        Point4::new(self.x, self.y, self.z, e4)
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct Point4 {
+    e1: i64,
+    e2: i64,
+    e3: i64,
+    e4: i64,
+}
+
+impl Point4 {
+    fn new(e1: i64, e2: i64, e3: i64, e4: i64) -> Point4 {
+        Point4 {
+            e1,
+            e2,
+            e3,
+            e4,
+        }
+    }
+
+    fn neighbors(&self) -> Vec<Point4> {
+        (-1..2)
+            .cartesian_product(-1..2)
+            .cartesian_product(-1..2)
+            .cartesian_product(-1..2)
+            .filter_map(|(((i, j), k), l)| {
+                if i != 0 || j != 0 || k != 0 || l != 0 {
+                    Some(Point4::new(self.e1 + i, self.e2 + j, self.e3 + k, self.e4 + l))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -86,6 +124,41 @@ pub fn solve_part1(input: &HashMap<Point, Cube>) -> usize {
 
     for _ in 0..6 {
         let reach: HashSet<Point> = space
+            .keys()
+            .map(|point| point.neighbors())
+            .flatten()
+            .collect();
+        space = reach
+            .iter()
+            .map(|point| {
+                let active_neighbors = point
+                    .neighbors()
+                    .iter()
+                    .filter(|point| *space.get(point).unwrap_or(&Cube::Inactive) == Cube::Active)
+                    .count();
+                let cube = *space.get(point).unwrap_or(&Cube::Inactive);
+                if cube == Cube::Active && active_neighbors == 2 || active_neighbors == 3 {
+                    (*point, Cube::Active)
+                } else if cube == Cube::Active {
+                    (*point, Cube::Inactive)
+                } else if cube == Cube::Inactive && active_neighbors == 3 {
+                    (*point, Cube::Active)
+                } else {
+                    (*point, Cube::Inactive)
+                }
+            })
+            .collect();
+    }
+
+    space.values().filter(|cube| **cube == Cube::Active).count()
+}
+
+#[aoc(day17, part2)]
+pub fn solve_part2(input: &HashMap<Point, Cube>) -> usize {
+    let mut space: HashMap<Point4, Cube> = input.iter().map(|(point, cube)| (point.to_point4(0), *cube)).collect();
+
+    for _ in 0..6 {
+        let reach: HashSet<Point4> = space
             .keys()
             .map(|point| point.neighbors())
             .flatten()
